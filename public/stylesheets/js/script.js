f5731c8192514b8d33f11401dd04402073d1c180
ayoafolalu <ayo.afolalu@yahoo.co.uk>
2020-07-22 20:02:54 +0000

    add stylesheets

---
+++ public/stylesheets/js/script.js
@@ -0,0 +1,12 @@
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
